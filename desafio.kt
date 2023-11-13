4ac10db9e823fe4ca34291ce4b3cd293c29bb155
luisrotter <113470450+luisrotter@users.noreply.github.com>
2023-11-13 16:52:55 -0300

    Ajustes realizados no código

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,6 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
+class Usuario(val id: Int, val nome: String, val email: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +9,24 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nome} matriculado com sucesso!")
+        } else {
+            println("Usuário ${usuario.nome} já está matriculado.")
+        }
+    }
+
+    override fun toString(): String {
+        return "Formação(nome='$nome', conteudos=$conteudos, inscritos=$inscritos)"
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario = Usuario(1, "João", "joao@gmail.com")
+    val conteudo = ConteudoEducacional("Kotlin Básico", 60)
+    val formacao = Formacao("Desenvolvimento Android", listOf(conteudo))
+
+    formacao.matricular(usuario)
+    println(formacao)
 }
